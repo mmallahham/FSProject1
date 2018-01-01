537acc7a50abe1892e34179a0f3ae7937a3854d6
mmallahham <mmallahham@hotmail.com>
2018-01-01 22:50:06 +0100

    jsdom package added

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,22 @@
 import {expect} from 'chai';
+import jsdom from "jsdom";
+import fs from "fs";
+
 
 describe("test the test package",() => {
   it("should pass",()=>{
     expect(true).to.equal(true);
+  });
+});
+
+describe("test the dom structor",()=>{
+  it("should have h1 with Users text",(done)=>{
+    const index = fs.readFileSync("./src/index.html","utf-8");
+    jsdom.env(index,(err,window)=>{
+      const h1 = window.document.getElementsByTagName("h1")[0];
+      expect(h1.innerHTML).to.equal("Users");
+      done();
+      window.close();
+    });
   });
 });
