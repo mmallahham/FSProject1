37e814c9efbb3c3871caaeae84500225038ee164
mmallahham <mmallahham@hotmail.com>
2018-01-01 14:58:22 +0100

    setting up webpack

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,9 +1,18 @@
 import express from 'express';
 var path = require('path');
 var open = require('open');
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
 
 var port = 3000;
 var app = express();
+const compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
+
 
 app.get('/', function(req, res) {
   console.log(__dirname);
@@ -13,7 +22,7 @@ app.get('/', function(req, res) {
 
 app.listen(port, function(err) {
   if (err) {
-    console.log(err);
+   // console.log(err);
   } else {
     open('http://localhost:' + port);
   }
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import "./index.css";
+
+debugger;
+console.log("fgdfgd fgfgdfg ");
