d84d109824b65797076ca22a6a98ccf66ddff89d
mmallahham <mmallahham@hotmail.com>
2018-01-02 22:27:24 +0100

    api

--- src/index.test.js
+++ src/index.test.js
@@ -2,6 +2,7 @@ import {expect} from 'chai';
 import jsdom from "jsdom";
 import fs from "fs";
 
+const { JSDOM } = jsdom;
 
 describe("test the test package",() => {
   it("should pass",()=>{
@@ -12,11 +13,12 @@ describe("test the test package",() => {
 describe("test the dom structor",()=>{
   it("should have h1 with Users text",(done)=>{
     const index = fs.readFileSync("./src/index.html","utf-8");
-    jsdom.env(index,(err,window)=>{
-      const h1 = window.document.getElementsByTagName("h1")[0];
-      expect(h1.innerHTML).to.equal("Users");
-      done();
-      window.close();
-    });
+    const { window } = new JSDOM(index);
+    const h1 = window.document.getElementsByTagName("h1")[0];
+    expect(h1.innerHTML).to.equal("Users");
+    done();
+    window.close();
   });
 });
+
+
